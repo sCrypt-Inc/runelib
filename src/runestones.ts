1d92318c114faf07e4111402eea66ce0d0cdd76c
hh <hf.zheng@scrypt.io>
2024-04-25 21:22:08 +0800

    add comments

--- src/runestones.ts
+++ src/runestones.ts
@@ -5,6 +5,10 @@ import { decodeLEB128, encodeLEB128 } from "./leb128";
 import { chunks, toPushData } from "./utils";
 import { getSpacersVal, removeSpacers } from "./spacers";
 
+
+/**
+ * Rune IDs are represented in text as BLOCK:TX.
+ */
 export class RuneId {
     constructor(public block: number, public idx: number) {
 
@@ -39,6 +43,13 @@ export class RuneId {
 }
 
 
+/**
+ * Rune ID block heights and transaction indices in edicts are delta encoded.
+ * Edict rune ID decoding starts with a base block height and transaction index of zero. 
+ * When decoding each rune ID, first the encoded block height delta is added to the base block height. 
+ * If the block height delta is zero, the next integer is a transaction index delta. 
+ * If the block height delta is greater than zero, the next integer is instead an absolute transaction index.
+ */
 export class Edict {
     constructor(public id: RuneId, public amount: bigint, public output: number) {
 
@@ -59,31 +70,57 @@ export class Edict {
     }
 }
 
+
 export enum Flag {
+    /** The Etching flag marks this transaction as containing an etching. */
     Etching = 0,
+    /** The Terms flag marks this transaction's etching as having open mint terms. */
     Terms = 1,
+    /** The Turbo flag marks this transaction's etching as opting into future protocol changes. These protocol changes may increase light client validation costs, or just be highly degenerate. */
     Turbo = 2,
+    /** The Cenotaph flag is unrecognized. */
     Cenotaph = 127,
 }
 
 export enum Tag {
+    /** The Body tag marks the end of the runestone's fields, causing all following integers to be interpreted as edicts. */
     Body = 0,
+    /** The Flag field contains a bitmap of flags, whose position is 1 << FLAG_VALUE: */
     Flags = 2,
+    /** The Rune field contains the name of the rune being etched. If the Etching flag is set but the Rune field is omitted, a reserved rune name is allocated. */
     Rune = 4,
+    /** The Premine field contains the amount of premined runes. */
     Premine = 6,
+    /** The Cap field contains the allowed number of mints. */
     Cap = 8,
+    /** The Amount field contains the amount of runes each mint transaction receives. */
     Amount = 10,
+    /** The HeightStart and HeightEnd fields contain the mint's starting and ending absolute block heights, respectively. The mint is open starting in the block with height HeightStart, and closes in the block with height HeightEnd. */
     HeightStart = 12,
     HeightEnd = 14,
+    /** The OffsetStart and OffsetEnd fields contain the mint's starting and ending block heights, relative to the block in which the etching is mined. The mint is open starting in the block with height OffsetStart + ETCHING_HEIGHT, and closes in the block with height OffsetEnd + ETCHING_HEIGHT. */
     OffsetStart = 16,
     OffsetEnd = 18,
+    /** The Mint field contains the Rune ID of the rune to be minted in this transaction. */
     Mint = 20,
+    /** The Pointer field contains the index of the output to which runes unallocated by edicts should be transferred. If the Pointer field is absent, unallocated runes are transferred to the first non-OP_RETURN output. */
     Pointer = 22,
+    /** The Cenotaph field is unrecognized. */
     Cenotaph = 126,
 
+    /** The Divisibility field, raised to the power of ten, is the number of subunits in a super unit of runes. */
     Divisibility = 1,
+
+    /** The Spacers field is a bitfield of • spacers that should be displayed between the letters of the rune's name. Trailing spacers are ignored. */
     Spacers = 3,
+    /** The Symbol field is the Unicode codepoint of the Rune's currency symbol, 
+     * which should be displayed after amounts of that rune. If a rune does not have a currency symbol,
+     * the generic currency character ¤ should be used. 
+     * For example, if the Symbol is # and the divisibility is 2, 
+     * the amount of 1234 units should be displayed as 12.34 #. 
+     */
     Symbol = 5,
+    /** The Nop field is unrecognized. */
     Nop = 127,
 }
 
